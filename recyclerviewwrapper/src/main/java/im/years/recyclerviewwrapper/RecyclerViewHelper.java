3d3179dea819123d4642410fd0556f710ba759e1
Alvin <didiao.zeng@foxmail.com>
2017-01-24 15:38:51 +0800

    Update RecyclerViewHelper.java

--- recyclerviewwrapper/src/main/java/im/years/recyclerviewwrapper/RecyclerViewHelper.java
+++ recyclerviewwrapper/src/main/java/im/years/recyclerviewwrapper/RecyclerViewHelper.java
@@ -249,7 +249,7 @@ public class RecyclerViewHelper {
         synchronized (this) {
             if (success) {
 
-                if ( isMore && （ requestPage == getCurrentPage() || requestPage > (getCurrentPage() + 1) ) ) {
+                if ( isMore && ( requestPage == getCurrentPage() || requestPage > (getCurrentPage() + 1) ) ) {
                     return;
                 }
 
